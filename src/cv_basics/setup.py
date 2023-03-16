2be87ef8f6cd27a99fb228ff92d54d721dfa1aea
qbaaa-0 <ntfr123@gmail.com>
2023-03-16 23:26:59 +0100

    added camera subscriber package

---
+++ src/cv_basics/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'cv_basics'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='qbaaa-0',
+    maintainer_email='ntfr123@gmail.com',
+    description='recieve image message from rpi camera and convert it to opencv, then find aruco markers',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rpi_cam_aruco = cv_basics.rpi_cam_aruco:main'
+        ],
+    },
+)
